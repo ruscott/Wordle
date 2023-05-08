53dd39babbb316995b8b07ad60c67879401fc015
Ruchika Scott <ru.scott24@gmail.com>
2023-05-08 12:57:13 +0100

    First draft wordle

--- wordle-app/src/App.styles.ts
+++ wordle-app/src/App.styles.ts
@@ -5,20 +5,7 @@ const Box = styled.input`
   margin-top: 5%;
 `;
 
-const Row = styled.div`
-  width: 50vh;
-  height: 10vh;
-  display: flex;
-  margin: auto;
-`;
 
-const LetterGuessBox = styled.input`
-  height: 95%;
-  width: 95%;
-  border: solid;
-  margin: auto;
-`;
-
-const styles = { Box, Row, LetterGuessBox };
+const styles = { Box };
 
 export default styles;
