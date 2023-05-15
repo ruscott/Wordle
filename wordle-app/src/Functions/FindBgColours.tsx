db269f02b1bbc2629907effea18c31431d074488
Ruchika Scott <ru.scott24@gmail.com>
2023-05-15 09:30:03 +0100

    Refactoring app.tsx

---
+++ wordle-app/src/Functions/FindBgColours.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { KeyboardColours } from "../Types";
+import { keyboardColoursInit } from "../WordGameInit/keyboardColours";
+
+export const findBgColours = (
+  guessedWord: string[],
+  secretWord: string[],
+  keyboardColours: KeyboardColours,
+  setKeyboardColours: (keyboardColours: KeyboardColours) => void
+) => {
+  let newKeyboardColours = keyboardColours;
+  let colours: string[] = ["", "", "", "", ""];
+  for (let i = 0; i < guessedWord.length; i++) {
+    const letter = guessedWord[i];
+    if (letter === " ") {
+      colours[i] = "white";
+    } else if (letter === secretWord[i]) {
+      // Correct letter in correct position
+      secretWord[i] = "_";
+      colours[i] = "green";
+      newKeyboardColours[letter] = "green";
+    }
+  }
+  for (let i = 0; i < guessedWord.length; i++) {
+    const letter = guessedWord[i];
+    if (colours[i] === "") {
+      if (secretWord.includes(letter)) {
+        secretWord[secretWord.indexOf(letter)] = "_";
+        colours[i] = "yellow";
+        if (newKeyboardColours[letter] !== "green") {
+          newKeyboardColours[letter] = "yellow";
+        }
+      } else {
+        newKeyboardColours[letter] = "grey";
+        colours[i] = "grey";
+      }
+    }
+  }
+  setKeyboardColours(newKeyboardColours);
+  return colours;
+};
